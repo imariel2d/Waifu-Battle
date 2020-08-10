5a46351196875c27ca82b9632fe0d32d8df9a916
Ariel <ImAriel2D@gmail.com>
2020-08-10 02:14:27 -0700

    Fixed data base issue

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/waifu-battle', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
